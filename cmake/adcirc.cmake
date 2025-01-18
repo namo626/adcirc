af928130d401d9545341a9eb26f0f153150307de
namo <namo26june@gmail.com>
2025-01-18 11:28:55 +0700

    Work

--- cmake/adcirc.cmake
+++ cmake/adcirc.cmake
@@ -55,7 +55,8 @@ if(BUILD_ADCIRC)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/internaltide.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/gl2loc_mapping.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/couple2baroclinic3D.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F)
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/dg.F)
 
   if(NETCDF_WORKING)
     set(ADCIRC_SOURCES
