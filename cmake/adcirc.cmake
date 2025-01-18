2f5c861da18c4930eca942310818df248f452984
namo <namo26june@gmail.com>
2025-01-18 10:29:19 +0700

    Add basic DG routines

--- cmake/adcirc.cmake
+++ cmake/adcirc.cmake
@@ -61,7 +61,11 @@ if(BUILD_ADCIRC)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/sun_moon_system.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/ephemerides.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/tidalpotential.F90
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F)
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/dg.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/slopelimiter.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/numerical_flux.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/dg_integration.F)
 
   if(NETCDF_WORKING)
     set(ADCIRC_SOURCES
@@ -76,6 +80,8 @@ if(BUILD_ADCIRC)
   endif()
 
   add_executable(adcirc ${ADCIRC_SOURCES})
+  set_source_files_properties(${CMAKE_CURRENT_SOURCE_DIR}/src/dg_integration.F
+    PROPERTIES COMPILE_FLAGS "-Wall -Wextra -fallow-argument-mismatch")
   set(ADCIRC_COMPILER_FLAGS "${ADDITIONAL_FLAGS_ADCIRC} ${ADCIRC_OPTION_FLAGS}")
   addcompilerflags(adcirc ${ADDITIONAL_FLAGS_ADCIRC})
   addnetcdflibraries(adcirc)
