de67721d7a33bf344318358ff9323a750fe16516
Namo <namo26june@gmail.com>
2025-01-24 15:57:18 -0600

    Add internal_edge

--- cmake/adcirc.cmake
+++ cmake/adcirc.cmake
@@ -63,7 +63,8 @@ if(BUILD_ADCIRC)
       ${CMAKE_CURRENT_SOURCE_DIR}/src/tidalpotential.F90
       ${CMAKE_CURRENT_SOURCE_DIR}/src/subgridLookup.F
       ${CMAKE_CURRENT_SOURCE_DIR}/src/dg.F
-      ${CMAKE_CURRENT_SOURCE_DIR}/src/slopelimiter.F)
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/slopelimiter.F
+      ${CMAKE_CURRENT_SOURCE_DIR}/src/numerical_flux.F)
 
   if(NETCDF_WORKING)
     set(ADCIRC_SOURCES
