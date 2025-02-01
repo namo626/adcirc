65bab4747fc7f771f5ec3d9db76ac83646ef1438
namo626 <namo26june@gmail.com>
2025-02-01 16:17:20 -0600

    Vectorize rhs_dg

--- cmake/adcirc.cmake
+++ cmake/adcirc.cmake
@@ -81,7 +81,7 @@ if(BUILD_ADCIRC)
 
   add_executable(adcirc ${ADCIRC_SOURCES})
   set_source_files_properties(${CMAKE_CURRENT_SOURCE_DIR}/src/dg_integration.F
-    PROPERTIES COMPILE_FLAGS "-Wall -Wextra -fallow-argument-mismatch")
+    PROPERTIES COMPILE_FLAGS "-O3 -fallow-argument-mismatch -fopt-info-vec-optimized -march=native")
   set(ADCIRC_COMPILER_FLAGS "${ADDITIONAL_FLAGS_ADCIRC} ${ADCIRC_OPTION_FLAGS}")
   addcompilerflags(adcirc ${ADDITIONAL_FLAGS_ADCIRC})
   addnetcdflibraries(adcirc)
