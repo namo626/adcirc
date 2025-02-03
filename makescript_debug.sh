a7b24bbf3343977836d16b1ebfb446e2a0f5f764
Namo <namo26june@gmail.com>
2025-02-03 17:46:34 -0600

    Add floating point check

--- makescript_debug.sh
+++ makescript_debug.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_Fortran_FLAGS_DEBUG="-Wall -Wextra -g -fbounds-check"
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_Fortran_FLAGS_DEBUG="-Wall -Wextra -ffpe-trap=invalid,zero,overflow -g -fbounds-check"
 
 cmake .. -DBUILD_ADCIRC=ON -DBUILD_PADCIRC=OFF -DBUILD_ADCPREP=OFF -DBUILD_PADCSWAN=OFF
