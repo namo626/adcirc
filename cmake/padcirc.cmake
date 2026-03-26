46d0229656185fb6cd6420d9a78a890e1785fa38
Namo <namo26june@gmail.com>
2026-03-26 10:56:54 -0500

    Change messenger_elem to 2008

--- cmake/padcirc.cmake
+++ cmake/padcirc.cmake
@@ -75,7 +75,7 @@ set(PADCIRC_SOURCES
     ${CMAKE_CURRENT_SOURCE_DIR}/src/slopelimiter.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/numerical_flux.F
     ${CMAKE_CURRENT_SOURCE_DIR}/src/dg_integration.F
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/messenger_elem.F
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/messenger_elem.F90
     ${CMAKE_CURRENT_SOURCE_DIR}/src/precipitation.F90)
 
 if(NETCDF_WORKING)
